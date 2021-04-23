e861e796e2d3ff8d7e015a5a0657bbcf1a4be111
aldo.lopes <aldo.lopes@192.168.0.21>
2021-04-23 00:16:52 -0300

    Added Test

---
+++ src/modulo8/JavaScriptExecutorTest.java
@@ -0,0 +1,45 @@
+package modulo8;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class JavaScriptExecutorTest {
+
+	public static void main(String[] args) throws Exception {
+		
+		// Seta a variavel webdriver.chrome.driver do sistema para a localização do Driver do Chrome que foi baixado
+		// Instancia o Driver do Chrome na variável driver
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\aldog\\Downloads\\chromedriver_win32\\chromedriver.exe" );
+		WebDriver driver = new ChromeDriver();
+		
+		// Adicionando Implicit Wait de 10 segundos para toda a sessão
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		// Navega para o Advantage Online Shopping
+		driver.get("https://www.advantageonlineshopping.com/");
+		
+		// Sleep 10 segundos
+	    Thread.sleep(10000);
+
+        //Creating the JavascriptExecutor interface object by Type casting		
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        
+        //Vertical scroll down by 600  pixels		
+        js.executeScript("window.scrollBy(0,800)");
+		
+		// Sleep 10 segundos
+	    Thread.sleep(10000);
+		
+		// Fechar pagina e browser
+		driver.close();
+		driver.quit();
+
+	}
+
+}
